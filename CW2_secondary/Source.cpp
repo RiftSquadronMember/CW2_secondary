e8ac75719f466ebbc2efae58555bc95f45885de3
Yarik Vodila <109005417+RiftSquadronMember@users.noreply.github.com>
2024-10-16 15:06:29 +0500

    Update Source.cpp

--- CW2_secondary/Source.cpp
+++ CW2_secondary/Source.cpp
@@ -6,6 +6,9 @@
 #include <ctime>
 #include <cmath>
 
+const int arr_size = 5;
+
+
 void text_visualise(std::string text, int avg_speed) {
 	for (int i = 0; i < text.length(); i++) {
 		std::cout << text[i];
@@ -20,46 +23,73 @@ void text_visualise(std::string text, int avg_speed) {
 }
 
 
+int Zapolnenie(int arr_int[]) {
+	for (int i = 0; i < arr_size; i++) {
+		arr_int[i] = rand() % 11 - 5;
+	}
+	return 0;
+}
+float Zapolnenie(float arr_float[]) {
+	for (int i = 0; i < arr_size; i++) {
+		arr_float[i] = rand() % 11 - 5;
+	}
+	return 0;
+}
+char Zapolnenie(char arr_char[]) {
+	
+	for (int i = 0; i < arr_size; i++) {
+		int rand_num = rand() % 3 + 1;
+		if (rand_num == 1) { arr_char[i] = 'A'; }
+		if (rand_num == 2) { arr_char[i] = 'B'; }
+		if (rand_num == 3) { arr_char[i] = 'C'; }
+	}
+	return 0;
+}
+
+
+void Vivod(int arr_int[]) {
+	std::cout << "int - ";
+	for (int i = 0; i < arr_size; i++) {
+		std::cout << arr_int[i];
+		if (i < arr_size - 1) {
+			std::cout << ", ";
+		}
+	}
+	
+}
+void Vivod(float arr_float[]) {
+	std::cout << "\nfloat - ";
+	for (int i = 0; i < arr_size; i++) {
+		std::cout << arr_float[i];
+		if (i < arr_size - 1) {
+			std::cout << ", ";
+		}
+	}
+	
+}
+void Vivod(char arr_char[]) {
+	std::cout << "\nchar - ";
+	for (int i = 0; i < arr_size; i++) {
+		std::cout << arr_char[i];
+		if (i < arr_size - 1) {
+			std::cout << ", ";
+		}
+	}
+}
+
 
 int main() {
-	std::cout << "fart" << std::endl;
-	int value = -1, answer = 0;
-	std::vector<int> numbers;
-	std::vector<int> numbers1;
-	std::vector<int> numbers2;
+	srand(time(0));
+	int arr_int[arr_size]{};
+	float arr_float[arr_size]{};
+	char arr_char[arr_size]{};
 
 
-	text_visualise("Enter any number of values, and then\nenter \"0\" to add them to each other\n\n", 25);
-	while (value != 0) {
-		text_visualise(">>> ", 10);
-		std::cin >> value;
-		numbers.push_back(value);
-	}
-	text_visualise("k, that's all, i guess.\nLet's see...\nOh, there it is!", 25);
-	for (int i = 0; i < numbers.size(); i++) {
-		answer = answer + numbers[i];
-		if (numbers[i] % 2 == 0) {
-			numbers2.push_back(numbers[i]);
-		}
-		else { numbers1.push_back(numbers[i]); }
-	}
-
-
-	text_visualise("\n\nSum of them --> ", 25);
-	std::cout << answer;
-	answer = 0;
-	for (int i = 0; i < numbers2.size(); i++) {
-		answer = answer + numbers2[i];
-	}
-
-	text_visualise("\nSum of even numbers --> ", 25);
-	std::cout << answer;
-
-	answer = 0;
-	for (int i = 0; i < numbers1.size(); i++) {
-		answer = answer + numbers1[i];
-	}
-
-	text_visualise("\nSum of odd numbers --> ", 25);
-	std::cout << answer;
+	
+	Zapolnenie(arr_int);
+	Zapolnenie(arr_float);
+	Zapolnenie(arr_char);
+	Vivod(arr_int);
+	Vivod(arr_float);
+	Vivod(arr_char);
 }
